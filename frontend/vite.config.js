562caec5c464bbf773eead59fe0d76b35eab846d
sachi1010 <sachisachi8567@gmail.com>
2025-06-12 20:08:26 +0530

    u

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+
+  base: './', // or '/your-subpath/' if hosted under a sub-directory
+  build: {
+    outDir: 'dist',
+  },
 })
+
