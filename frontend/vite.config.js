744278b104d462e9e800bc3e67dbe07dde5a775e
sachi1010 <sachisachi8567@gmail.com>
2025-06-29 11:35:53 +0530

    up

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 
   base: './', // or '/your-subpath/' if hosted under a sub-directory
   build: {
-    outDir: 'build',
-  },
+    outDir: 'dist'  // must be "dist" for Vercel unless you customize settings
+  }
 })
 
